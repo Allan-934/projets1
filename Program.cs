e33251e1668ce658fbc138e53301f951f5c6ee46
Decugis Allan <allan.decugis@edouardgand.fr>
2026-03-09 16:54:32 +0100

    exo1 fin

--- Program.cs
+++ Program.cs
@@ -1,7 +1,42 @@
 ﻿namespace projets1;
 
+ public class Student
+    {
+        public int id;
+        public static int Id;
+         public string name;
+         public float average;
+         public bool isScholarshipHolder ;       
+
+         public Student() // 1er constructeur sans paramètre  
+    {
+        
+    }
+     public Student(string name, float average, bool isScholarshipHolder) // 2eme constructeur complet 
+    {
+         this.name = name;
+         this.average = average;
+         this.isScholarshipHolder = isScholarshipHolder;
+    }
+    public Student(string name, float average) // 3eme constructeur partiel (name, average)
+    {
+        this.name = name ;
+        this.average = average ;
+        Id ++;
+    }
+     
+     public static int Id()
+    {
+        return Id; 
+    }
+
+    }
+
+
+
 class Program
 {
+   
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
