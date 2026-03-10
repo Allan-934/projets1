793167e24d8618aa0617c204e9ba52f83ab48ec4
Decugis Allan <allan.decugis@edouardgand.fr>
2026-03-10 11:32:39 +0100

    Exercice 4 à 7

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Common;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+using System.Net.Mail;
 
 namespace projets1;
 
@@ -43,12 +45,85 @@ namespace projets1;
     }
 
     }
+public class Course // Exercice 3 
+{
+    public int id;
+    public string name; 
+    public float credits;
+    public bool isMandatory;
+    public List<Student> students = new List<Student>(); 
+
+    public Course() // 1er Construteur sans paramètre Exercice 4 
+    {
+        
+    }
+
+    public Course (string name, float credits, bool isMandatory) //2eme constructeur complet
+    {
+        this.name = name;
+        this.credits = credits;
+        this.isMandatory = isMandatory;
+    }
+
+    public void AddStudent (Student student)
+    {
+        if (student != null)
+        {
+            this.students.Add(student);
+        }
+    }
+
+    public Course (string name) // 3eme constructeur simplifié
+    {
+        this.name = name;
+    }
+
+    public void InfoCours() // Exercice 7 bis 
+    {
+        string obligatoire = this.isMandatory ? "Oui" : "Non";
+        Console.WriteLine($"Nom : {name}, Crédits : {credits}, obligatoire : {obligatoire}, Nombre d'élèves :{students.Count}");
+    }
+}
 
 class Program
 {
    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Student s1 = new Student(); // Exercice 2 Création des élèves
+        s1.Name = "Alice";
+        s1.Average = 14.5f;
+        Student s2 = new Student ("Bernard", 15.7f, true);
+        Student s3 = new Student ("Emma", 15.7f);
+        Student s4 = new Student ("Lucas", 10.3f, true);
+        Student s5 = new Student ("Sarah", 5.5f, false);
+        Student s6 = new Student ();
+
+        Course c1 = new Course ("Mathématiques", 50.5f, true); // Exercice 5 
+        Course c2 = new Course ("informatique", 40f, true); // Constructeur complet
+        Course c3 = new Course ("Anglais"); // Construteur smplifié
+        Course c4 = new Course (); // Constructeur sans paramètre
+        Course c5 = new Course ("Histoire", 20f, false); 
+
+        c1.AddStudent(s1); // Exercice 6 Inscription des étudiants aux cours 
+        c1.AddStudent(s2);
+        c1.AddStudent(s3);
+
+        c2.AddStudent(s2);
+        c2.AddStudent(s4);
+
+        c3.AddStudent(s1);
+        c3.AddStudent(s5);
+
+        c5.AddStudent(s4);
+
+        c1.InfoCours(); //Exercice 7 Afficher tout les cours
+        c2.InfoCours();
+        c3.InfoCours();
+        c4.InfoCours();
+        c5.InfoCours();
+
+        
+
     }
 }
