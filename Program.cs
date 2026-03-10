1baa03672989db72d931d22916eb770ee70651ad
Decugis Allan <allan.decugis@edouardgand.fr>
2026-03-10 09:54:00 +0100

    Fin de l'exercice 1

--- Program.cs
+++ Program.cs
@@ -1,38 +1,48 @@
-﻿namespace projets1;
+﻿using System.Data.Common;
+
+namespace projets1;
 
  public class Student
     {
         public int id;
-        public static int Id;
+        public static int Id = 1; // Id que l'on va incrémenté a chaque élève 
          public string name;
          public float average;
          public bool isScholarshipHolder ;       
 
          public Student() // 1er constructeur sans paramètre  
     {
-        
+        this.id = Id++; 
     }
      public Student(string name, float average, bool isScholarshipHolder) // 2eme constructeur complet 
     {
-         this.name = name;
-         this.average = average;
+         this.Name = name;
+         this.Average = average;
          this.isScholarshipHolder = isScholarshipHolder;
+         this.id = Id++;
     }
     public Student(string name, float average) // 3eme constructeur partiel (name, average)
     {
-        this.name = name ;
-        this.average = average ;
-        Id ++;
+        this.Name = name ;
+        this.Average = average ;
+        this.id = Id++;
     }
-     
-     public static int Id()
+    public string Name
     {
-        return Id; 
+        get {return name;} // lire la valeur de name
+        set {name = value ;} // mettre la valeur attribué à name 
+    }
+    public float Average
+    {
+        get {return average;} // lire la valeur average 
+        set {average = value;} // mettre la valeur attribué average
+    }
+    public static int GetCurrentCount() // Méthode pour appeller l'Id à chaque incrémentation 
+    {
+        return Id;
     }
 
     }
-
-
 
 class Program
 {
--- obj/Debug/net10.0/projets1.AssemblyInfo.cs
+++ obj/Debug/net10.0/projets1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projets1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e33251e1668ce658fbc138e53301f951f5c6ee46")]
 [assembly: System.Reflection.AssemblyProductAttribute("projets1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projets1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
