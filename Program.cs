31249c5671450a53c7c65ce4b27522ec99517167
Decugis Allan <allan.decugis@edouardgand.fr>
2026-03-11 13:46:03 +0100

    Fin du TP 1 sans l'exo 13

--- Program.cs
+++ Program.cs
@@ -83,13 +83,25 @@ public class Course // Exercice 3
         string obligatoire = this.isMandatory ? "Oui" : "Non";
         Console.WriteLine($"Nom : {name}, Crédits : {credits}, obligatoire : {obligatoire}, Nombre d'élèves :{students.Count}");
 
-        if (students.Count > 0) // Exercice 8 pas finis 
+        if (students.Count > 0) // Exercice 8  condition  si il y a un etudiant  
         {
-            Console.WriteLine("List of Students:"); 
-            foreach (Student s in students)
+            Console.WriteLine("Liste des étudiants:"); 
+            foreach (Student s in students) //boucle foreach qui va parcourrir la liste et afficher l'id et le nom de l'étudiant
             {
-                Console.WriteLine($"- [ID {s.id}] {s.Name}")
+                Console.WriteLine($"- [ID {s.id}] {s.Name}");
             }
+        }
+        else
+        {
+            Console.WriteLine("Aucun étudaunt inscrit");
+        }
+    }
+        public void RemoveStudent(Student student)// Exercice 12 – Méthode pour supprimer un étudiant
+
+    {
+        if (student != null)
+        {
+            this.students.Remove(student);
         }
     }
 }
@@ -126,13 +138,44 @@ class Program
 
         c5.AddStudent(s4);
 
-        c1.InfoCours(); //Exercice 7 Afficher tout les cours
+        Console.WriteLine("Détails de tout les cours"); //Exercice 7 Afficher les details d'un cours
+        c1.InfoCours(); 
         c2.InfoCours();
         c3.InfoCours();
         c4.InfoCours();
         c5.InfoCours();
 
-        
+        Console.WriteLine("Listes des cours obligatoires"); // exercice 9 Afficher les cours obligatoires
+        List<Course> allCourse = new List<Course> {c1, c2, c3, c4, c5, }; 
+        foreach (Course c in allCourse)
+        {
+            if (c.isMandatory)
+            {
+                Console.WriteLine($"- {c.name}");
+            }
+            
+        }
+        Console.WriteLine("\n=== ÉTUDIANTS BOURSIERS ===");// Exercice 10 – Identifier les étudiants boursiers 
+        List<Student> allStudents = new List<Student> { s1, s2, s3, s4, s5, s6 };
+        foreach (Student s in allStudents)
+        {
+            if (s.isScholarshipHolder)
+            {
+                Console.WriteLine($"- {s.Name} est boursier.");
+            }
+        }
+        Console.WriteLine("\n=== ÉTUDIANTS AVEC UNE MOYENNE > 15 ===");// Exercice 11 – Utiliser la moyenne 
+        foreach (Student s in allStudents)
+        {
+            if (s.Average > 15f)
+            {
+                Console.WriteLine($"- {s.Name} a une excellente moyenne ({s.Average}/20)");
+            }
+        }
+
+        c1.RemoveStudent(s2); // Exercice 12 – Supprimer un étudiant d’un cours 
+        Console.WriteLine("\n=== APRÈS SUPPRESSION DE BERNARD DU COURS C1 ===");
+        c1.InfoCours();
 
     }
 }
--- obj/Debug/net10.0/projets1.AssemblyInfo.cs
+++ obj/Debug/net10.0/projets1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projets1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e33251e1668ce658fbc138e53301f951f5c6ee46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7e387c6db1f403c95b3de20e43816bea649fbee")]
 [assembly: System.Reflection.AssemblyProductAttribute("projets1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projets1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
